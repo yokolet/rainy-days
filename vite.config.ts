ab0e5dae28642d69f7f61754544f6f77daae1fff
Yoko Harada <yokolet@gmail.com>
2025-01-19 21:29:27 +0900

    adds vue with typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
-
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     vue(),
+    vueDevTools(),
   ],
 })
