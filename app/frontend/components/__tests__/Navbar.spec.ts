56e4b00e42216e9131c6473fa39ba4dc435bd600
Yoko Harada <yokolet@gmail.com>
2025-01-25 12:54:46 +0900

    improves Navbar spec condidering the login logic

--- app/frontend/components/__tests__/Navbar.spec.ts
+++ app/frontend/components/__tests__/Navbar.spec.ts
@@ -1,38 +1,47 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest'
-import { RouterLinkStub, shallowMount, VueWrapper } from '@vue/test-utils'
+import { mount, RouterLinkStub, shallowMount, VueWrapper } from '@vue/test-utils'
 import Navbar from '../Navbar.vue'
 import User from '../User.vue'
 import { createTestingPinia } from '@pinia/testing'
 
-const createWrapper = () => shallowMount(Navbar ,{
-  global: {
-    stubs: {
-      RouterLink: RouterLinkStub,
-    },
-    plugins: [createTestingPinia({ stubActions: false, createSpy: vi.fn})],
-  },
-});
-let wrapper = createWrapper();
-
 describe('Navbar.vue', () => {
-  beforeEach(() => {
-    wrapper = createWrapper();
-  })
-
   it('has router links', () => {
+    const wrapper = shallowMount(Navbar, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+        plugins: [createTestingPinia({ createSpy: vi.fn})],
+      },
+    });
     const paths: string[] = ['/', '/about'];
     wrapper.findAllComponents(RouterLinkStub)
       .map((link: VueWrapper, index: number) => expect(link.props().to).toBe(paths[index]))
   })
 
   it('shows login button before auth', () => {
+    const wrapper = shallowMount(Navbar, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+        plugins: [createTestingPinia({ createSpy: vi.fn})],
+      },
+    });
     expect(wrapper.find('#login').isVisible()).toBe(true)
     expect(wrapper.html()).not.toContain('Logout')
     expect(wrapper.findComponent(User).isVisible()).toBe(false)
   })
 
-  it('shows logout button and user info after auth', async () => {
-    await wrapper.find('#login').trigger('click')
+  it('shows logout button and user info after auth', () => {
+    const wrapper = mount(Navbar, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+        plugins: [createTestingPinia({ createSpy: vi.fn, initialState: { auth: { isAuthenticated: true }} })],
+      },
+    });
 
     expect(wrapper.html()).not.toContain('Login')
     expect(wrapper.find('#logout').isVisible()).toBe(true)
