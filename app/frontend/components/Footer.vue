0478f97fbaab55f339dc13cfc19802d30484341b
Yoko Harada <yokolet@gmail.com>
2025-01-26 15:21:28 +0900

    adds a gitlab icon in the footer

--- app/frontend/components/Footer.vue
+++ app/frontend/components/Footer.vue
@@ -5,7 +5,9 @@
   <footer class="sticky top-[100vh] bg-[#dfebe7] text-center dark:bg-[#2e3231]">
     <div class="bg-black/5 p-4 text-center text-surface dark:text-white text-sm md:text-base">
       <span class="pr-4">© 2025 Yoko Harada</span>
-      <span> visit <a class="font-bold" href="https://gitlab.com/yokolet/rainy-days">Rainy Days</a> repo</span>
+      <span> visit <a class="font-bold hover:underline" href="https://gitlab.com/yokolet/rainy-days">
+        <font-awesome-icon :icon="['fab', 'gitlab']" /> Rainy Days
+      </a> repo</span>
     </div>
   </footer>
 </template>
