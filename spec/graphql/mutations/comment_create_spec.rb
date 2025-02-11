d2004705eb939e7bfdd2b9e0c05c9f2c3c6dd9a5
Yoko Harada <yokolet@gmail.com>
2025-02-11 16:29:56 +0900

    creates post and comment mutation specs

---
+++ spec/graphql/mutations/comment_create_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "comment create", type: :graphql do
+  context "mutation" do
+    it "should raise exception without authenticated user" do
+      context = {
+        current_user: nil
+      }
+      attrs = attributes_for(:comment)
+      variables = {
+        "body": attrs[:body],
+        "pid": 0,
+        "rid": nil
+      }
+      result = RainyDaysSchema.execute(query, variables: variables, context: context)
+      expect(result["errors"].first.dig("extensions", "code")).to eql(GraphqlErrorType::AUTHENTICATION_REQUIRED)
+    end
+
+    it "should return created post" do
+      user = create(:user)
+      context = {
+        current_user: user
+      }
+      attrs = attributes_for(:comment)
+      post = create(:post, user_id: user.id)
+      variables = {
+        "body": attrs[:body],
+        "pid": post[:id]
+      }
+      result = RainyDaysSchema.execute(query, variables: variables, context: context)
+      expect(result.dig("data", "commentCreate", "comment", "id")).not_to be_nil
+      expect(result.dig("data", "commentCreate", "comment", "body")).to eql(attrs[:body])
+      expect(result.dig("data", "commentCreate", "comment", "userId")).not_to be_nil
+      expect(result.dig("data", "commentCreate", "comment", "email")).to eql(user.email)
+      expect(result.dig("data", "commentCreate", "comment", "updatedAt")).not_to be_nil
+    end
+  end
+
+  def query
+    <<-gql
+    mutation comment($body: String!, $pid: ID!, $rid: ID) {
+      commentCreate(input: {
+        body: $body,
+        postId: $pid,
+        replyId: $rid
+      }) {
+        comment {
+          id
+          body
+          postId
+          userId
+          email
+          updatedAt
+        }
+      }
+    }
+    gql
+  end
+end
---
+++ spec/graphql/mutations/post_create_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "post create", type: :graphql do
+  context "mutation" do
+    it "should raise exception without authenticated user" do
+      context = {
+        current_user: nil
+      }
+      attrs = attributes_for(:post)
+      variables = {
+        "title": attrs[:title],
+        "content": attrs[:content]
+      }
+      result = RainyDaysSchema.execute(query, variables: variables, context: context)
+      expect(result["errors"].first.dig("extensions", "code")).to eql(GraphqlErrorType::AUTHENTICATION_REQUIRED)
+    end
+
+    it "should return created post" do
+      user = create(:user)
+      context = {
+        current_user: user
+      }
+      attrs = attributes_for(:post)
+      variables = {
+        "title": attrs[:title],
+        "content": attrs[:content]
+      }
+      result = RainyDaysSchema.execute(query, variables: variables, context: context)
+      expect(result.dig("data", "postCreate", "post", "id")).not_to be_nil
+      expect(result.dig("data", "postCreate", "post", "title")).to eql(attrs[:title])
+      expect(result.dig("data", "postCreate", "post", "user", "id")).not_to be_nil
+      expect(result.dig("data", "postCreate", "post", "user", "email")).to eql(user.email)
+      expect(result.dig("data", "postCreate", "post", "user", "provider")).to eql(user.provider)
+    end
+  end
+
+  def query
+    <<-gql
+    mutation post($title: String!, $content: String!) {
+      postCreate(input: {
+        title: $title,
+        content: $content
+      }) {
+        post {
+          id
+          title
+          content
+          updatedAt
+          user {
+            id
+            email
+            provider
+          }
+        }
+      }
+    }
+    gql
+  end
+end
