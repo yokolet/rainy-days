c550b4d0bdff239b01d3632def9a83ddfef250c4
Yoko Harada <yokolet@gmail.com>
2025-03-02 22:32:08 +0900

    updates seeds.rb to create nested comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,72 +53,103 @@ def seedPosts(emails, providers)
   posts = [
     {
       user_id: user_ids[0],
-      title: "The Daffodil Sky",
-      content: "Architecto atque quia. Voluptates ullam aut. Repudiandae delectus explicabo."
+      title: "For ever and a day.",
+      content: "All the world's a stage, and all the men and women merely players; they have their exits and their entrances, and one man in his time plays many parts, his acts being seven ages."
     },
     {
       user_id: user_ids[1],
-      title: "Recalled to Life",
-      content: "Harum dolor impedit. Quo iste officia. Et ab nulla."
+      title: "I will speak daggers to her but use none.",
+      content: "Doubt thou the stars are fire, doubt that the sun doth move, doubt truth to be a liar, but never doubt I love."
     },
     {
       user_id: user_ids[2],
-      title: "Fair Stood the Wind for France",
-      content: "Mollitia et consectetur. Et porro harum. Beatae facere quisquam."
+      title: "An honest tale speeds best, being plainly told.",
+      content: "Conscience is but a word that cowards use, devised at first to keep the strong in awe."
     },
     {
       user_id: user_ids[3],
-      title:  "Down to a Sunless Sea",
-      content: "Vero architecto quia. Et tenetur exercitationem. Dolor laborum corrupti."
+      title:  "Tempt not a desperate man.",
+      content: "Good night, good night! Parting is such sweet sorrow, that I shall say good night till it be morrow."
+    },
+    {
+      user_id: user_ids[4],
+      title:  "Wisely and slow; they stumble that run fast.",
+      content: "See how she leans her cheek upon her hand. Oh, that I were a glove upon that hand that I might touch that cheek!"
     },
   ]
 
   Post.upsert_all(posts)
 end
 
-def seedComments(emails, providers)
-  user_ids = emails.zip(providers).map do |email, p|
-    User.where(email: email, provider: p).first.id
-  end
-  post_ids = Post.where(user_id: user_ids).map {|e| e.id}
+def seedComments
+  user_ids = User.all.map {|u| u.id}    # five users exist
+  post_ids = Post.all.map {|e| e.id}    # five posts exist
   comments = [
     {
       user_id: user_ids[0],
-      post_id: post_ids[3],
-      body: "Quaerat accusantium eum dicta."
+      post_id: post_ids[0],
+      body: "The highest function of ecology is understanding consequences."
     },
     {
       user_id: user_ids[1],
-      post_id: post_ids[2],
-      body: "Dolorum veniam qui laborum."
+      post_id: post_ids[0],
+      body: "The mystery of life isn't a problem to solve, but a reality to experience."
     },
     {
       user_id: user_ids[2],
-      post_id: post_ids[1],
-      body: "Ipsam quisquam voluptatem iure."
+      post_id: post_ids[0],
+      body: "But I can tell you, dear God, for the father, nothing."
     },
     {
       user_id: user_ids[3],
-      post_id: post_ids[0],
-      body: "Debitis hic consequatur voluptatem."
+      post_id: post_ids[1],
+      body: "Bring in that floating fat man, the Baron!"
     },
-  ]
-  Comment.upsert_all(comments)
-  first_comments = Comment.where(post_id: post_ids)
-  replies = [
     {
-      user_id: user_ids[1],
-      post_id: first_comments[0].post_id,
-      reply_id: first_comments[0].id,
-      body: "Qui est omnis laudantium.",
+      user_id: user_ids[4],
+      post_id: post_ids[1],
+      body: "And you, my son, are you one who gives or one who takes?"
+    },
+    {
+      user_id: user_ids[3],
+      post_id: post_ids[2],
+      body: "I made my peace gesture. The forms of kanly have been obeyed!"
     },
     {
       user_id: user_ids[2],
-      post_id: first_comments[1].post_id,
-      reply_id: first_comments[1].id,
-      body: "Non qui beatae iusto.",
+      post_id: post_ids[4],
+      body: "Knowing where the trap is—that's the first step in evading it."
+    },
+  ]
+  Comment.upsert_all(comments)
+
+end
+
+def seedReplies
+  user_ids = User.all.map {|u| u.id}    # five users exist
+  post_ids = Comment.select(:post_id).group(:post_id).having("count(comments.id) > 1").map {|c| c.post_id}
+  comments_0 = Comment.where(post_id: post_ids[0])
+  replies = [
+    {
+      user_id: user_ids[0],
+      post_id: post_ids[0],
+      reply_id: comments_0[0].id,
+      body: "My dear Piter, your pleasures are what tie you to me. How could I object to that?",
+    },
+    {
+      user_id: user_ids[1],
+      post_id: post_ids[0],
+      reply_id: comments_0[1].id,
+      body: "The spice must flow",
     }
   ]
+  comments_1 = Comment.where(post_id: post_ids[1])
+  replies << {
+    user_id: user_ids[2],
+    post_id: post_ids[1],
+    reply_id: comments_1[0].id,
+    body: "Bring in that floating fat man, the Baron!",
+  }
   Comment.upsert_all(replies)
 end
 
@@ -130,4 +161,5 @@ usernames = ["Amber", "dannette.rau", "clemencia",
 
 seedUsers(emails, providers, usernames)
 seedPosts(emails, providers)
-seedComments(emails, providers)
+seedComments()
+seedReplies()
