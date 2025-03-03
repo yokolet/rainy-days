69086c2ec45656573b7fc0002969773c1f7e6cfe
Yoko Harada <yokolet@gmail.com>
2025-03-03 13:34:25 +0900

    controls cookies to make those consistent

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if !user_signed_in?
+      cookies.delete("rainy-days", {
+        value: "",
+        expires: 10.years.ago,
+        path: "/",
+        httponly: false
+      })
+    end
+  end
+
+  private
+  def user_signed_in?
+    session[:email] && session[:email].length >= 3
   end
 end
