ee8d70cc77c890a5984c4a6b4c8ca65cf204dbe5
Yoko Harada <yokolet@gmail.com>
2025-03-03 21:08:40 +0900

    updates about content using markdown to html converter

---
+++ app/frontend/assets/about.md
@@ -0,0 +1,42 @@
+## ~ On calm rainy days, surrounded by soft sound of pouring rain and feeling fresh air, people are blogging ~
+
+<br />
+<br />
+
+## What is this application
+
+This is a portfolio app by Ruby on Rails and Vue.js to showcase my skill set.
+
+<br />
+<br />
+
+## How to Use
+- Everybody can see all posts and comments regardless of login status.
+- To write a post, comment or reply, login by Google, GitHub or GitLab.
+- Comments can be added to posts and other comments.
+- Toggle light and dark mode by the button at top-right corner.
+- The screen can be resized, and contents are visible in any size.
+
+<br />
+<br />
+
+## Technologies
+- Backend
+  - Ruby on Rails: version 8.0.1
+  - PostgreSQL
+  - RSpec
+  - GraphQL Ruby
+  - Docker
+
+- Frontend
+  - Vue.js: version 3.5.13
+  - Vite
+  - Bun
+  - TypeScript
+  - Vue Router
+  - VueUse
+  - Pinia
+  - Vue Apollo
+  - Tailwind CSS
+  - Font Awesome
+
--- app/frontend/components/About.vue
+++ app/frontend/components/About.vue
@@ -1,31 +1,25 @@
 <script setup lang="ts">
+import { marked } from 'marked'
+import aboutContent from '../assets/about.md?raw'
+import { computed } from 'vue';
 
+const about = computed(() => marked.parse(aboutContent))
 </script>
 
 <template>
   <section id="about-app">
-    <div class="text-sm bg-white dark:bg-zinc-700 text-zinc-900 dark:text-teal-100 font-roboto-mono m-4 p-4 rounded-md">
-      <dl class="flex flex-col space-y-4">
-        <dt>This is a sample Ruby on Rails application with Vue.js on the frontend.</dt>
-        <dt>
-          <span class="font-semibold">Usage</span>
-          <ul class="font-light">
-            <li><font-awesome-icon :icon="['fas', 'mug-hot']" /> Everybody can see posts and comments</li>
-            <li><font-awesome-icon :icon="['fas', 'mug-hot']" /> When a user logs in, the user can write a post and comment</li>
-          </ul>
-        </dt>
-        <dt>
-          <span class="font-semibold">Technologies and Versions</span>
-          <ul class="font-light">
-            <li><font-awesome-icon :icon="['fas', 'screwdriver-wrench']" /> Ruby on Rails: 8.0.1</li>
-            <li><font-awesome-icon :icon="['fas', 'screwdriver-wrench']" /> Vue.js: 3.5.13</li>
-          </ul>
-        </dt>
-      </dl>
+    <div class="text-sm bg-white dark:bg-zinc-700 text-teal-950 dark:text-teal-50 font-roboto-mono m-4 p-4 rounded-md">
+      <div v-html="about"></div>
     </div>
   </section>
 </template>
 
 <style scoped>
+#about-app :deep(h2) {
+  @apply text-gray-800 dark:text-gray-200 font-bold text-base;
+}
 
+#about-app :deep(ul) {
+  @apply list-disc ml-4;
+}
 </style>
